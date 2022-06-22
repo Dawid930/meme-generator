381dc8970a65892f851c719c91b427da6a068b7e
Dawid930 <davidnyikes@gmail.com>
2022-06-22 10:43:47 +0200

    code cleanup

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -6,48 +6,47 @@ import image from "../Images/troll-face.png"
 
 export default function Meme() {
 
-    const memeImg = useRef(null)
+    const memeImg = useRef(null);
 
     const [meme, setMeme] = useState({
         topText: "",
         bottomText: "",
         randomImage: "http://i.imgflip.com/1bij.jpg" 
-    })
-    const [allMemes, setAllMemes] = useState([])
+    });
+    const [allMemes, setAllMemes] = useState([]);
     
-
     
     useEffect(() => {
         fetch("https://api.imgflip.com/get_memes")
             .then(res => res.json())
             .then(data => setAllMemes(data.data.memes))
-    }, [])
+    }, []);
     
 
     function getMemeImage() {
-        const randomNumber = Math.floor(Math.random() * allMemes.length)
-        const url = allMemes[randomNumber].url
+        const randomNumber = Math.floor(Math.random() * allMemes.length);
+        const url = allMemes[randomNumber].url;
         setMeme(prevMeme => ({
             ...prevMeme,
             randomImage: url
-        }))
-    }
+        }));
+    };
     
 
     function handleChange(event) {
-        const {name, value} = event.target
+        const {name, value} = event.target;
         setMeme(prevMeme => ({
             ...prevMeme,
             [name]: value
-        }))
-    }
+        }));
+    };
 
 
     const handleCaptureClick = useCallback(async () => {
-        const canvas = await html2canvas(memeImg.current, {allowTaint : true, useCORS : true})
-        const dataURL = canvas.toDataURL('image/jpg')
-        downloadjs(dataURL, 'download.jpg', 'image/jpg')
-    }, [])
+        const canvas = await html2canvas(memeImg.current, {allowTaint : true, useCORS : true});
+        const dataURL = canvas.toDataURL('image/jpg');
+        downloadjs(dataURL, 'download.jpg', 'image/jpg');
+    }, []);
     
 
     return (
@@ -93,5 +92,5 @@ export default function Meme() {
                 <img src= {image} className="disclaimer--trollface" alt="trollface"/>
             </div>
         </main>
-    )
-}
+    );
+};
